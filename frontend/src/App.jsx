10e2de13e74eb535113b5f6b3ba8dc1dc301e9c0
abdul2005-tech <abdullahmutahir08@gmail.com>
2026-05-21 02:23:53 +0530

    phase6 updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,76 @@
-import AppRoutes from "./routes/AppRoutes";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Items from "./pages/Items";
+import Alerts from "./pages/Alerts";
+import ConnectDevice from "./pages/ConnectDevice";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
 
-    return <AppRoutes />;
+  return (
+
+    <Routes>
+
+      {/* PUBLIC ROUTES */}
+
+      <Route
+        path="/"
+        element={<Login />}
+      />
+
+      <Route
+        path="/signup"
+        element={<Signup />}
+      />
+
+      {/* PROTECTED ROUTES */}
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+
+      <Route
+        path="/items"
+        element={
+          <ProtectedRoute>
+            <Items />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/alerts"
+        element={
+          <ProtectedRoute>
+            <Alerts />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/connect-device"
+        element={
+          <ProtectedRoute>
+            <ConnectDevice />
+          </ProtectedRoute>
+        }
+      />
+
+    </Routes>
+  );
 }
 
 export default App;
